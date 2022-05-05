abb509b4f28420b01f07b185b548b6777513e356
rafa1326 <phillyaryeh@gmail.com>
2022-05-05 13:27:55 -0400

    add innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-  
-  
-var createTaskHandler = function() {
-  
-    event.preventDefault();
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
+  
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-  formEl.addEventListener("submit", createTaskHandler);
+  
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+ 
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
